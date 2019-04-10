0dff5b526fefcbadf0129b3b1525b3e7efe3718f
OdisseusIsBack <epostboxxx@gmail.com>
2019-04-10 17:38:54 +0300

    Refactored WinnerController

--- src/com/xo/game/Controller/WinnerController.java
+++ src/com/xo/game/Controller/WinnerController.java
@@ -8,55 +8,92 @@ import java.awt.*;
 
 public class WinnerController {
 
-    public Figure getWinner(final Field field) throws InvalidPointException{
-        Figure winner = null;
-        try {
-            //rows check
-            for (int i = 0; i < field.getSize(); i++) {
-                if (check(field, new Point(i, 0), new Point(i, 1), new Point(i, 2))) {
-                    winner = field.getFigure(new Point(i, 0));
-                    return winner;
-                }
-            }
-            //columns check
-            for (int i = 0; i < field.getSize(); i++) {
-                if (check(field, new Point(0, i), new Point(1, i), new Point(2, i))) {
-                    winner = field.getFigure(new Point(0, i));
-                    return winner;
-                }
-            }
-            //diagonal1 check
-            if (check(field, new Point(0, 0), new Point(1, 1), new Point(2, 2))) {
-                winner = field.getFigure(new Point(0, 0));
-                return winner;
-            }
-            //diagonal2 check
-            if (check(field, new Point(0, 2), new Point(1, 1), new Point(2, 0))) {
-                winner = field.getFigure(new Point(1, 1));
-                return winner;
-            }
-
-        }catch (InvalidPointException e){
-            e.printStackTrace();
+    public Figure getWinner(final Field field) {
+        Figure winner;
+        for (int i = 0; i < field.getSize(); i++) {
+            winner = checkRow(field, i);
+            if (winner != null) return winner;
+            winner = checkColumn(field, i);
+            if (winner != null) return winner;
         }
-        return winner;
+        winner = checkDiag1(field);
+        if (winner != null) return winner;
+        winner = checkDiag2(field);
+        if (winner != null) return winner;
 
+        return null;
     }
 
-    private boolean check(final Field field, final Point p1, final Point p2, final Point p3){
+    private Figure checkDiag1(final Field field) {
+        final Point p1 = new Point(0, 0);
+        final Point p2 = new Point(1, 1);
+        final Point p3 = new Point(2, 2);
+
         try {
-            if (field.getFigure(p1) == null){
-                return false;
+            if (field.getFigure(p1) != null && field.getFigure(p2) != null &&
+                    field.getFigure(p3) != null &&
+                    field.getFigure(p1).equals(field.getFigure(p2)) &&
+                    field.getFigure(p1).equals(field.getFigure(p3))) {
+                return field.getFigure(p1);
             }
-            if (field.getFigure(p1) == field.getFigure(p2) && field.getFigure(p1) == field.getFigure(p3)){
-                return false;
-            }
-        }catch (InvalidPointException e) {
-            e.printStackTrace();
+        } catch (final InvalidPointException e) {
+            return null;
         }
-        return false;
+        return null;
     }
 
+    private Figure checkDiag2(final Field field) {
 
+        final Point p1 = new Point(2, 0);
+        final Point p2 = new Point(1, 1);
+        final Point p3 = new Point(0, 2);
 
+        try {
+            if (field.getFigure(p1) != null && field.getFigure(p2) != null
+                    && field.getFigure(p3) != null &&
+                    field.getFigure(p1).equals(field.getFigure(p2)) &&
+                    field.getFigure(p1).equals(field.getFigure(p3))) {
+                return field.getFigure(p1);
+            }
+        } catch (final InvalidPointException e) {
+            return null;
+        }
+        return null;
+    }
+
+    private Figure checkColumn(final Field field, final Integer i) {
+        final Point p1 = new Point(0, i);
+        final Point p2 = new Point(1, i);
+        final Point p3 = new Point(2, i);
+
+        try {
+            if (field.getFigure(p1) != null && field.getFigure(p2) != null
+                    && field.getFigure(p3) != null &&
+                    field.getFigure(p1).equals(field.getFigure(p2)) &&
+                    field.getFigure(p1).equals(field.getFigure(p3))) {
+                return field.getFigure(p1);
+            }
+        } catch (final InvalidPointException e) {
+            return null;
+        }
+        return null;
+    }
+
+    private Figure checkRow(final Field field, final Integer i) {
+        final Point p1 = new Point(i, 0);
+        final Point p2 = new Point(i, 1);
+        final Point p3 = new Point(i, 2);
+
+        try {
+            if (field.getFigure(p1) != null && field.getFigure(p2) != null
+                    && field.getFigure(p3) != null &&
+                    field.getFigure(p1).equals(field.getFigure(p2)) &&
+                    field.getFigure(p1).equals(field.getFigure(p3))) {
+                return field.getFigure(p1);
+            }
+        } catch (final InvalidPointException e) {
+            return null;
+        }
+        return null;
+    }
 }
